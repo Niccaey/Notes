a816811ccf2ac2c58aff525faf316b83aed5c9ca
Nicole Riedler <n.riedler@gmx.at>
2023-09-25 01:04:07 +0200

    index.html: class="notes" in id="notes" geändert index.js: Änderungen newNote, Fkt speichern hinzugefügt, Fkt laden versucht hinzuzufügen index.css: Anpassungen entsprechenden der Änderungen im HTML und JS

--- index.js
+++ index.js
@@ -1,35 +1,58 @@
 "use strict"
 
-/*window.onload = loadNote;
-
-function init() {
-        document.getElementById("btn1").addEventListener('click', newNote);
-}*/
+window.onload = laden();
 
 function newNote() {
-        var neu = document.querySelector(".neu");
-        var note = document.createElement("div");
-        var h2 = document.createElement("h2");        
-        var text = document.createElement("textarea");
-        /*var btn1 = document.createElement("button");
-        var btn2 = document.createElement("button");*/
 
-        note.className = "note";
-        h2.textContent = "Notiz";
+        var titel = window.prompt("Füge einen Titel für deine Notiz ein.", ""); //Var erstellt um darin Ü zu speichern
+        //var notiz = window.prompt("Füge den Inhalt deiner Notiz ein.", "");
+        var note = document.getElementById("notes"); //Var erstellt, damit "Children" angehängt werden können
+        var noteElem = document.createElement("div");
+        var h2 = document.createElement("h2");       
+        var textarea = document.createElement("textarea");
+        var btn2 = document.createElement("button");
+        var btn3 = document.createElement("button");
+        var br = document.createElement("br");
 
-        note.appendChild(h2);
-        note.appendChild(text);
-       /* note.appendChild(btn1);
-        note.appendChild(btn2);*/
-        neu.appendChild(note);
+        btn2.id = "btn2";
+        btn3.id = "btn3";
 
+        noteElem.className = "note"; //CSS ".note" hinzugefügt
+        h2.className = "h2"; //CSS ".h2" hinzugefügt
+        h2.textContent = titel;
+        textarea.className = "text";
+        //textarea.textContent = notiz;
+        btn2.textContent = "+";
+        btn3.textContent = "loeschen";
 
+        btn2.setAttribute("onclick", "speichern()");
+        //btn3.addEventListener("click", loeschen); 
+
+        note.appendChild(noteElem);
+        noteElem.appendChild(h2);
+        noteElem.appendChild(textarea);
+        noteElem.appendChild(br);
+        noteElem.appendChild(btn2);
+        noteElem.appendChild(btn3);
 }
-
 
 function speichern() {
-        const textarea = document.getElementById("text");
-        const text = textarea.value;
-        localStorage.setItem("gespeicherterText", text);
+        var elem1 = document.querySelector(".h2");
+        var elem2 = document.querySelector(".text");
+
+        var notizObj = {
+                h2: elem1.textContent,
+                text: elem2.value
+        };
+
+        localStorage.setItem("notiz", JSON.stringify(notizObj));//JSON.stringify = konvertiert JS-Objekte in JSON-Strings
 }
 
+function laden() {
+        var elem2 = document.getElementsByClassName("notes");
+        var elem1 = localStorage.getItem("notiz");
+
+        if(elem1) {
+               elem2.value = JSON.parse(elem1);
+        }
+}
