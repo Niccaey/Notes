6330a5f19ea31414adf55078c754db42b9825044
Nicole Riedler <n.riedler@gmx.at>
2023-09-27 10:11:04 +0200

    index.html: Favicon hinzugefügt index.js: Ergänzt um die Funktionen loadNotes, deleteNotes, var auf let geändert, btn2 und btn3 bewusst unterschiedliche Methoden zum Funktionsaufruf gewählt

--- index.js
+++ index.js
@@ -1,58 +1,96 @@
 "use strict"
+window.onload = loadNotes;
 
-window.onload = laden();
-
-function newNote() {
-
-        var titel = window.prompt("Füge einen Titel für deine Notiz ein.", ""); //Var erstellt um darin Ü zu speichern
-        //var notiz = window.prompt("Füge den Inhalt deiner Notiz ein.", "");
-        var note = document.getElementById("notes"); //Var erstellt, damit "Children" angehängt werden können
-        var noteElem = document.createElement("div");
-        var h2 = document.createElement("h2");       
-        var textarea = document.createElement("textarea");
-        var btn2 = document.createElement("button");
-        var btn3 = document.createElement("button");
-        var br = document.createElement("br");
-
-        btn2.id = "btn2";
-        btn3.id = "btn3";
-
-        noteElem.className = "note"; //CSS ".note" hinzugefügt
-        h2.className = "h2"; //CSS ".h2" hinzugefügt
-        h2.textContent = titel;
-        textarea.className = "text";
-        //textarea.textContent = notiz;
-        btn2.textContent = "+";
-        btn3.textContent = "loeschen";
-
-        btn2.setAttribute("onclick", "speichern()");
-        //btn3.addEventListener("click", loeschen); 
-
-        note.appendChild(noteElem);
-        noteElem.appendChild(h2);
-        noteElem.appendChild(textarea);
-        noteElem.appendChild(br);
-        noteElem.appendChild(btn2);
-        noteElem.appendChild(btn3);
-}
-
-function speichern() {
-        var elem1 = document.querySelector(".h2");
-        var elem2 = document.querySelector(".text");
-
-        var notizObj = {
-                h2: elem1.textContent,
-                text: elem2.value
-        };
-
-        localStorage.setItem("notiz", JSON.stringify(notizObj));//JSON.stringify = konvertiert JS-Objekte in JSON-Strings
-}
-
-function laden() {
-        var elem2 = document.getElementsByClassName("notes");
-        var elem1 = localStorage.getItem("notiz");
-
-        if(elem1) {
-               elem2.value = JSON.parse(elem1);
+function loadNotes() {
+        let savedNotes = JSON.parse(localStorage.getItem("noteCont"));
+    
+        if (savedNotes) {
+                for (let i = 0; i < savedNotes.length; i++) {
+                        createNote(savedNotes[i].title, savedNotes[i].content);
+                }
         }
 }
+
+function createNote(title, content) {
+        let notes = document.getElementById("notes");
+
+        let liElem = document.createElement("li");
+        liElem.className = "note";
+
+        let h2 = document.createElement("h2");
+        h2.className = "h2";
+        h2.textContent = title;
+
+        let textarea = document.createElement("textarea");
+        textarea.className = "content";
+        textarea.textContent = content;
+
+
+        let btn2 = document.createElement("button");
+        btn2.id = "add";
+        btn2.textContent = "+";
+        btn2.addEventListener("click", saveNote);
+
+        let btn3 = document.createElement("button");
+        btn3.id = "delete";
+        btn3.textContent = "x";
+        btn3.onclick = deleteNote;
+
+        let br = document.createElement("br");
+
+        notes.appendChild(liElem);
+        liElem.appendChild(h2);
+        liElem.appendChild(textarea);
+        liElem.appendChild(br);
+        liElem.appendChild(btn2);
+        liElem.appendChild(btn3);
+}
+
+function addNote() {
+        let title = window.prompt("Füge einen Titel für deine Notiz ein.", "");
+        let content = window.prompt("Füge deine Notiz ein.", "");
+
+        if(title || content) {
+                createNote(title, content);
+        }
+}
+
+function saveNote() {
+        let hElem = document.querySelector(".h2");
+        let contentElem = document.querySelector(".content");
+       
+        let noteObj = {
+                title: hElem.textContent,
+                content: contentElem.value
+        };
+
+        let savedNotes = [];
+        savedNotes = JSON.parse(localStorage.getItem("noteCont"));
+        savedNotes.push(noteObj);
+
+        localStorage.setItem("noteCont", JSON.stringify(savedNotes));//JSON.stringify = konvertiert JS-Objekte in JSON-Strings
+}
+
+function deleteNote() {
+        let hElem = document.querySelector(".h2"); 
+        let contentElem = document.querySelector(".content"); 
+    
+        let savedNotes = JSON.parse(localStorage.getItem("noteCont"));
+    
+        // Durchsuchen Sie die gespeicherten Notizen, um die zu löschende Notiz zu finden
+        for (let i = 0; i < savedNotes.length; i++) {
+            if (savedNotes[i].title === hElem.textContent && savedNotes[i].content === contentElem.value) {
+                var indexToDelete = i;
+                break; // Wir haben die zu löschende Notiz gefunden, brechen Sie die Schleife ab.
+            }
+        }
+    
+        // Wenn der Index zum Löschen gültig ist, entfernen Sie die Notiz aus dem Array
+        if (indexToDelete !== -1) {
+            savedNotes.splice(indexToDelete, 1);
+            localStorage.setItem("noteCont", JSON.stringify(savedNotes));
+            
+            // Entfernen Sie das HTML-Element der Notiz aus dem DOM
+            this.parentElement.remove();
+        }
+    }
